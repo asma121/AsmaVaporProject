254e581a2c493b4078884ecdc1b59809d83ef0a7
asmaa_alshareef <81930013+asma121@users.noreply.github.com>
2021-12-31 00:52:41 +0300

    .

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,18 @@ func routes(_ app: Application) throws {
         return "It works!"
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("myTasks") { req ->[Task] in
+       
+        return [Task( objective: "first task" , created_at: Date()) , Task(objective: "seconed task" , created_at: Date())]
     }
+    
+    app.post("myTasks") { req -> Task in
+        let task = try? req.content.decode(Task.self)
+        return task!
+    }
+}
+
+struct Task : Content {
+    let objective : String
+    let created_at : Date
 }
